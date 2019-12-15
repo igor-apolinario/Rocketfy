3f2a15f72ccd08680beb3e92101bc1f54fbd4d46
Igor Apolinário <igor_apolinario96@hotmail.com>
2019-12-15 17:15:58 -0300

    Realizada criação da estrutura base do projeto

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from '../../styles/styles'
+
+export default function Header() {
+  return (
+    <div>
+        <Container>
+          <h1>Rocketfy</h1>
+        </Container>
+    </div>
+  )
+}
